dd1d31bb2d0564b2e7401e0a7353a1a596b3f670
Aleme Gabriel <gabriel.aleme@longbridgetech.com>
2021-10-07 14:06:44 +0100

    them colors added

---
+++ src/constants/color.js
@@ -0,0 +1,5 @@
+export const PRIMARY_COLOR = '#4a6bff';
+export const BACKGROUND_COLOR = '#f6faff';
+export const ACCENT_COLOR = '#5cccef';
+export const TEXT_COLOR = '#050626';
+export const LIGHT_TEXT_COLOR = '#d3d2d5';
